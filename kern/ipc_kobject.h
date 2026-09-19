3ea410e1c3bfd15ad597d170d2c27e0827198231
Leonardo Lopes Pereira <leonardolopespereira@outlook.com>
2026-09-19 11:29:39 +0200

    cleanup: remove unused IKOT_IO_DONE_QUEUE kobject type
    No port is typed or switched on as an IO done queue.

--- kern/ipc_kobject.h
+++ kern/ipc_kobject.h
@@ -68,7 +68,6 @@ typedef unsigned int ipc_kobject_type_t;
 #define IKOT_PAGING_NAME	16
 #define IKOT_HOST_SECURITY	17
 #define	IKOT_LEDGER		18
-#define IKOT_IO_DONE_QUEUE	22
 #define IKOT_SEMAPHORE		23
 #define IKOT_LOCK_SET		24
 #define IKOT_CLOCK		25
