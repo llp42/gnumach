076a91f3e899343b3745c7ae48e9dcdecbb9c58c
Leonardo Lopes Pereira <leonardolopespereira@outlook.com>
2026-09-19 10:52:49 +0200

    cleanup: remove unused IO_SPARE_START op flag
    The spare-flag range is never named; drivers that need their own bits
    would have to define them anyway.

--- device/io_req.h
+++ device/io_req.h
@@ -113,8 +113,6 @@ struct io_req {
 #define IO_INBAND	0x00004000	/* mig call was inband */
 #define	IO_LOANED	0x00010000	/* ior loaned by another module */
 
-#define	IO_SPARE_START	0x00020000	/* start of spare flags */
-
 /*
  * Standard completion routine for io_requests.
  */
